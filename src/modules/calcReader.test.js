71de9a2ae9e79a50d2f942b4bf00ace335b143df
James Foley <jdjfoley@gmail.com>
2022-07-17 20:24:23 -0700

    [jest/calcReader] add cleanup +nits

--- src/modules/calcReader.test.js
+++ src/modules/calcReader.test.js
@@ -1,10 +1,17 @@
 const { initCalcReader, startCalcReader, provideInputToCalcReader, closeReader } = require("./calcReader")
 var { calc, getStack, getLast } = require('./calculator')
-let mockExit;
-let mockLog;
+let mockExit
+let mockLog
+
 beforeAll(() => {
-    mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { });
-    mockLog = jest.spyOn(console, 'log').mockImplementation(() => { });
+    mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { })
+    mockLog = jest.spyOn(console, 'log')
+        .mockImplementation(() => { }); //comment this to see the logs instead
+})
+
+afterEach(() => {
+    //ensure no readers are left accidently listening
+    closeReader()
 })
 
 test('reader getLast in stack of [10, 20, 30]', () => {
